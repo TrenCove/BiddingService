be62f9fa927a005f1ead5a2d1f8f93767e7a39dd
dokdek <dokdek@gmail.com>
2023-03-24 02:14:43 -0400

    Fixed buy/bid functions and integrated with other services

---
+++ functions/buy.ts
@@ -0,0 +1,24 @@
+import sqlite3 from "sqlite3";
+import { itemDbRow } from "../interfaces/interfaces";
+
+const db = new sqlite3.Database("../db/items.db", (error) => {
+  if (error) {
+    console.error(error.message);
+  }
+  console.log("Connected to items database");
+});
+
+export async function BuyNow(bidder: string, id: number): Promise<itemDbRow | undefined> {
+  return new Promise((resolve, reject) => {
+    const stmt = "UPDATE items SET top_bidder=?, active=? WHERE item_id=? RETURNING *";
+    db.get(stmt, [`${bidder}`, 'false', `${id}`], (err, row: itemDbRow) => {
+      if (err) {
+        console.log("Error updating DB");
+        console.log(err);
+        reject(undefined);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+}
