ac88a63789115bc2f88a1051a2e54372bec0dfbc
chrismoon-code <christopher.moon2008@gmail.com~>
2023-03-24 13:34:57 -0400

    docv2

--- functions/buy.ts
+++ functions/buy.ts
@@ -7,7 +7,12 @@ const db = new sqlite3.Database("../db/items.db", (error) => {
   }
   console.log("Connected to items database");
 });
-
+/**
+ * This function allows the user to buy an item (Dutch auction)
+ * @param bidder - the user
+ * @param id - item id of the item that is being auctioned
+ * @returns 
+ */
 export async function BuyNow(bidder: string, id: number): Promise<itemDbRow | undefined> {
   return new Promise((resolve, reject) => {
     const stmt = "UPDATE items SET top_bidder=?, active=? WHERE item_id=? RETURNING *";
